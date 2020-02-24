f91d8dbf2df0b64fab40e0861c1ed5048050ae08
n7commanderjohn <kimgears2@gmail.com>
2020-02-24 13:37:07 -0500

    Chinese Localization added.

--- Don't_Starve/developed_mods/fast_equipment_revamped/modinfo.lua
+++ Don't_Starve/developed_mods/fast_equipment_revamped/modinfo.lua
@@ -1,7 +1,7 @@
-name = "Fast Equipment Revamped"
-version = "1.3.1"
-description = "Hamlet update. Machetes will now be in equip bar for Hamlet and RoG - Hamlet compatible worlds. Most related Hamlet items added to hotkey bars. Adds buttons over the inventory that give a fast access to the best main tools, weapons, armors, helmets and light resources. Besides optionally adds keybinds for the mentioned tools."
-author = "IceGrog; N7 Commander John (Hamlet Update)"
+name = "Fast Equipment Revamped / 快捷装备栏"
+version = "1.3.2"
+description = "Hamlet update. Machetes will now be in equip bar for Hamlet and RoG - Hamlet compatible worlds. Most related Hamlet items added to hotkey bars. Adds buttons over the inventory that give a fast access to the best main tools, weapons, armors, helmets and light resources. Besides optionally adds keybinds for the mentioned tools. \ 哈姆雷特更新！兼容哈姆雷特\n1.砍刀现在在哈姆雷特和巨人国中可以在装备栏装备。2.大多数哈姆雷特相关的项目可以添加到热键栏增加快捷按钮3.能够快速访问最好的主要工具，武器，盔甲，头盔和光源。4.可以给工具添加按键绑定。"
+author = "IceGrog; N7 Commander John (Hamlet Update); 途安 (Chinese Localization)"
 forumthread = "files/file/1992-fast-equipment-revamped/"
 icon = "modicon.tex"
 icon_atlas = "modicon.xml"
@@ -19,150 +19,150 @@ local string = ""
 
 local FIRST_NUMBER = 48
 for i = 1, 10 do
-	local ch = string.char(FIRST_NUMBER + i - 1)
-	keyslist[i] = {description = ch, data = ch}
+  local ch = string.char(FIRST_NUMBER + i - 1)
+  keyslist[i] = {description = ch, data = ch}
 end
 
 local FIRST_LETTER = 65
 for i = 11, 36 do
-	local ch = string.char(FIRST_LETTER + i - 11)
-	keyslist[i] = {description = ch, data = ch}
+  local ch = string.char(FIRST_LETTER + i - 11)
+  keyslist[i] = {description = ch, data = ch}
 end
 
-keyslist[37] = {description = "DISABLED", data = false}
+keyslist[37] = {description = "DISABLED / 禁用", data = false}
 
 numbers = {}
 for i = 0, 5 do
-	numbers[i+1] = {description = i, data = i}
+  numbers[i+1] = {description = i, data = i}
 end
 
 configuration_options = {
   {
     name = "Key_Axe",
-    label = "Axe's Keybind",
+    label = "Axe / 斧头按键",
     default = "1",
     options = keyslist
   },
   {
     name = "Key_Machete",
-    label = "Machete's Keybind",
+    label = "Machete / 砍刀按键",
     default = "6",
     options = keyslist
   },
   {
     name = "Key_Pickaxe",
-    label = "Pickaxe's Keybind",
+    label = "Pickaxe / 鹤嘴锄按键",
     default = "2",
     options = keyslist
   },
   {
     name = "Key_Shovel",
-    label = "Shovel's Keybind",
+    label = "Shovel / 铲子按键",
     default = "3",
     options = keyslist
   },
   {
     name = "Key_Hammer",
-    label = "Hammer's Keybind",
+    label = "Hammer / 锤子按键",
     default = "4",
     options = keyslist
   },
   {
-	  name = "Key_Pitchfork",
-    label = "Pitchfork's Keybind",
+    name = "Key_Pitchfork",
+    label = "Pitchfork / 草叉按键",
     default = "5",
     options = keyslist
   },
   {
     name = "Key_Light",
-    label = "Light Source's Keybind",
+    label = "Light / 光源按键",
     default = "X",
     options = keyslist
   },
   {
     name = "Key_Cane",
-    label = "Cane's Keybind",
+    label = "Cane / 行走手杖",
     default = "C",
     options = keyslist
   },
   {
     name = "Key_Weapon",
-    label = "Weapon's Keybind",
+    label = "Weapon / 武器按键",
     default = "F",
     options = keyslist
   },
   {
     name = "Key_Armor",
-    label = "Armor's Keybind",
+    label = "Armor / 护甲按键",
     default = "V",
     options = keyslist
   },
   {
     name = "Key_Helmet",
-    label = "Helmet's Keybind",
+    label = "Helmet / 头盔按键",
     default = "G",
     options = keyslist
   },
   {
     name = "Letters",
-    label = "Letters on Buttons",
+    label = "Letters on Buttons / 按钮显示",
     default = false,
     options = {
-		{description = "NO", data = false},
-		{description = "YES", data = true}
-	}
+      {description = "NO", data = false},
+      {description = "YES", data = true}
+    }
   },
   {
     name = "Disable_Keys",
-    label = "Disable Keybinds",
+    label = "Disable Keybinds / 禁用快捷键",
     default = false,
     options = {
-		{description = "NO", data = false},
-		{description = "YES", data = true}
-	}
+      {description = "NO", data = false},
+      {description = "YES", data = true}
+    }
   },
   {
     name = "Disable_Buttons",
-    label = "Disable Buttons",
+    label = "Disable Buttons / 禁用按键",
     default = false,
     options = {
-		{description = "NO", data = false},
-		{description = "YES", data = true}
-	}
+      {description = "NO", data = false},
+      {description = "YES", data = true}
+    }
   },
   {
     name = "Support_Archery",
-    label = "Support Archery Mod",
+    label = "Support Archery Mod / 支持弓箭模组",
     default = false,
     options = {
-		{description = "NO", data = false},
-		{description = "YES", data = true}
-	}
+      {description = "NO", data = false},
+      {description = "YES", data = true}
+    }
   },
   {
     name = "Support_Scythes",
-    label = "Support Scythes Mod",
+    label = "Support Scythes Mod / 支持镰刀模组",
     default = false,
     options = {
-		{description = "NO", data = false},
-		{description = "YES", data = true}
-	}
+      {description = "NO", data = false},
+      {description = "YES", data = true}
+    }
   },
   {
     name = "Key_Scythe",
-    label = "Scythe's Keybind",
+    label = "Scythe / 镰刀按键",
     default = false,
     options = keyslist
   },
   {
     name = "Vertical_Offset",
-    label = "Vertical Offset",
+    label = "Vertical Offset / 垂直偏移",
     default = 0,
     options = numbers
   },
   {
     name = "Key_Refresh",
-    label = "Refresh Key",
+    label = "Refresh Key / 刷新键",
     default = false,
     options = keyslist
   },
